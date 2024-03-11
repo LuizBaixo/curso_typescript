52154604ae23e6c998b24c095e4a6b1fe8990200
Baixo <1072221661@ulife.com.br>
2024-03-11 19:12:41 -0300

    feat: aula-07

---
+++ src/aula-07.ts
@@ -0,0 +1,16 @@
+const funcaoCallback = (valor: number): void => {
+  console.log("DEU BOM" + valor);
+};
+
+const funcaoTeste2 = (valor1: number, valor2: number, callback: (n: number) => void): string => {
+  if (valor1 > valor2) {
+    callback(valor1);
+  }
+  return "Não deu Bom";
+};
+
+funcaoTeste2(29, 32, funcaoCallback);
+funcaoTeste2(654, 32, funcaoCallback);
+funcaoTeste2(43, 32, (x: number) => {
+  console.log("outra funcao" + x);
+});
