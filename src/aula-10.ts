b7b04d247afa12d86eec6193d3ca0c8cec43ac89
Baixo <1072221661@ulife.com.br>
2024-03-12 19:28:46 -0300

    feat: aula-10

---
+++ src/aula-10.ts
@@ -0,0 +1,17 @@
+enum MarcaTenis2 {
+  NIKE = 4,
+  DSAF = "dsaf",
+  FDSAF = "fdsaf",
+}
+
+interface Tenis {
+  marcaTenis: MarcaTenis2;
+}
+
+const test: Tenis = {
+  marcaTenis: MarcaTenis2.NIKE,
+};
+
+if (test.marcaTenis === MarcaTenis2.NIKE) {
+  console.log("é nike");
+}
